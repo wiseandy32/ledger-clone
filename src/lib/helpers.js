0727080f5fec43d7ed6dc7a4f914ac11499dd34b
wiseandy32 <wiseandy32@gmail.com>
2024-11-28 21:43:28 +0100

    add region input select and avatar update

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -54,6 +54,46 @@ export const filterCountries = (
     : filteredCountries;
 };
 
+export const filterRegions = (
+  regions = [],
+  priorityRegions = [],
+  whitelist = [],
+  blacklist = []
+) => {
+  let regionsListedFirst = [];
+  let filteredRegions = regions;
+
+  if (whitelist.length > 0) {
+    filteredRegions = regions.filter(
+      ({ shortCode }) => whitelist.indexOf(shortCode) > -1
+    );
+  } else if (blacklist.length > 0) {
+    filteredRegions = regions.filter(
+      ({ shortCode }) => blacklist.indexOf(shortCode) === -1
+    );
+  }
+
+  if (priorityRegions.length > 0) {
+    // ensure the Regions are added in the order in which they are specified by the user
+    priorityRegions.forEach((slug) => {
+      const result = filteredRegions.find(
+        ({ shortCode }) => shortCode === slug
+      );
+      if (result) {
+        regionsListedFirst.push(result);
+      }
+    });
+
+    filteredRegions = filteredRegions.filter(
+      ({ shortCode }) => priorityRegions.indexOf(shortCode) === -1
+    );
+  }
+
+  return regionsListedFirst.length
+    ? [...regionsListedFirst, ...filteredRegions]
+    : filteredRegions;
+};
+
 export const getSingleDocument = async (uid, queryKey = "uid") => {
   const usersRef = collection(db, "users");
   const document = query(usersRef, where(`${queryKey}`, "==", uid));
